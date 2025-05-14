c42c84f0034900af55ce6bc63ced09e74f2c7f67
BravoDario <adriandariobravo@gmail.com>
2025-05-14 17:52:24 -0600

    Apartado History responsivo y traducciones al francés del sitio

--- src/data/events.js
+++ src/data/events.js
@@ -3,11 +3,12 @@ import { date } from "astro:schema";
 export const events = [
   {
     title: "Electricity Transformation Canada",
-    description:
-      "El negocio de la energía, transformado.",
+    description_es: "El negocio de la energía, transformado.",
     description_en: "The business of energy, transformed.",
-    date: "6 al 8 de Octubre de 2026",
+    description_fr: "Le secteur de l'énergie, transformé.",
+    date_es: "6 al 8 de Octubre de 2026",
     date_en: "October 6-8, 2026",
+    date_fr: "Du 6 au 8 octobre 2026",
     location: "Enercare Centre, Toronto, Ontario, Canadá",
     image: "/img/events/elec_transf_canada.webp",
     link: "https://electricity-transformation.ca/register/",
