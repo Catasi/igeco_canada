58734ac1b6a0f86bffd5cdb12e18323d0d5f1181
BravoDario <adriandariobravo@gmail.com>
2025-05-15 12:16:02 -0600

    "Tarjetas Eventos Nacionales e Internacionales (se agregó el componente de tarjeta)"

---
+++ src/data/InternationalEvents.js
@@ -0,0 +1,44 @@
+export const events = [
+  {
+    title: "RE+ MÉXICO",
+    description_es:
+      "La feria del futuro energético renovable de México y América Latina",
+    description_en:
+      "America Lanita and Mexico's future renewable energy festival",
+    description_fr:
+      "La fête du futur énergie renouvelable de México et Amérique latine",
+    date_es: "14 al 16 de Abril de 2026",
+    date_en: "April 14-16, 2026",
+    date_fr: "Du 14 au 16 avril 2026",
+    location: "Expo Guadalajara, Guadalajara, Jalisco, México",
+    image: "/img/events/replus2026.webp",
+    link: "https://re-plus-mexico.com.mx",
+  },
+  {
+    title: "Industrial Transformation Mexico",
+    description_es: "El evento más influyente para la Industria 4.0",
+    description_en: "The most influential event for Industry 4.0",
+    description_fr: "L'événement le plus influent pour l'Industrie 4.0",
+    date_es: "11 al 13 de Noviembre de 2025",
+    date_en: "November 11-13, 2025",
+    date_fr: "Du 11 au 13 novembre 2025",
+    location: "Centro CityBanamex, CDMX, México",
+    image: "/img/events/Logo_ITM.webp",
+    link: "https://industrialtransformation.mx",
+  },
+  {
+    title: "America's Mobility of the Future",
+    description_es:
+      "Junto con Expo & Summit abordan los principales desafíos que enfrenta la industria de movilidad.",
+    description_en:
+      "Together with Expo & Summit, we discuss the main challenges facing the mobility industry.",
+    description_fr:
+      "Ensamble avec Expo & Summit, nous discutons les principaux défis qui confrontent l'industrie de la mobilité.",
+    date_es: "11 al 13 de Noviembre de 2025",
+    date_en: "November 11-13, 2025",
+    date_fr: "Du 11 au 13 novembre 2025",
+    location: "Centro CityBanamex, CDMX, México",
+    image: "/img/events/amof2025logo.webp",
+    link: "https://amofexpo.igeco.mx",
+  },
+];
